5602768f92eceda239630474c90fe47ae82612bd
501711376 <31967587+501711376@users.noreply.github.com>
2018-03-31 22:16:35 -0400

    changes

---
+++ chapter 9.py
@@ -0,0 +1,12 @@
+# Question 1
+q=("x","y","apple")
+def pass1():
+    if q[2] == "apple" :
+        print("yes")
+    else:
+        print("no")
+pass1()
+# it's argument
+#question 2 : pair is a generalization of a  tuple
+
+#question 3: I think pair is a kind of tuple
